284749dae574104788a96b7ec7e7e56ad0045adf
zedtopvibes <zedtopvibes@gmail.com>
2026-04-20 21:20:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 export default {
   async fetch(request, env, ctx) { 
     const url = new URL(request.url);
-    const path = url.pathname;
+    const path = url.pathname; 
     
     // Homepage
     if (path === '/') {
